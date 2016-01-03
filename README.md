5544262800e21bbbe7c6067a9e48f6dd1a1a6e2c
Einari Kurvinen <einari.kurvinen@gmail.com>
2016-01-03 11:09:28 +0200

    upadated vagrant config, added more packages and fixed nginx php issues

--- README.md
+++ README.md
@@ -1,8 +1,17 @@
+##Prerequisite
+- Virtualbox
+- Vagrant (Remeber to run command: vagrant plugin install vagrant-vbguest)
+- SSH command line client (for windows)
+
 ## Vagrant Provisioning for Codiad
 
 This repository holds a simple setup of the Codiad Cloud IDE project.
 
-This will provision an Ubuntu 12.04 (64 bit) server with nginx and php-fpm.
+It is modified to include also node, meteor, grunt, bower, git and meteor. It should be a full stack to start developing node/meter webapps in cloud.
+
+If you like to exclude or include more packages, you can do so in ./vagrant/provision.sh. Just comment out packages you don't need or add some more, if you like.
+
+This will provision an Ubuntu 15.04 (64 bit) server with nginx and php-fpm.
 
 Unless you alter the Vagrantfile, the port will be 8080, meaning the application will be available at:
 
--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -4,11 +4,12 @@ echo "Updating"
 sudo apt-get update
 
 echo "Installing Packages"
-sudo apt-get install -y git-core php5-dev zip unzip nginx php5-fpm
+sudo apt-get install -y git-core php5 php5-cgi zip unzip nginx php5-fpm
 
 echo "Setting up nginx"
 sudo rm /etc/nginx/sites-enabled/default >> /dev/null
 sudo cp /vagrant/vagrant/codiad.conf /etc/nginx/sites-enabled/codiad
+sudo cp /vagrant/vagrant/www.conf /etc/php5/fpm/pool.d/
 
 if [ ! -d "/var/www" ]; then
     echo "Making /var/www"
@@ -27,9 +28,41 @@ fi
 
 echo "Setting Codiad permissions to www-data:www-data"
 sudo chown -R www-data:www-data /var/www/codiad
+sudo chown -R www-data:www-data /vagrant
+sudo chown www-data:www-data /var/run/php5-fpm.sock
 
 echo "Restarting nginx and php5-fpm"
 sudo service nginx restart
 sudo service php5-fpm restart
+
+echo "Installmongodb"
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+sudo echo "deb http://repo.mongodb.org/apt/ubuntu "$(lsb_release -sc)"/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list
+sudo apt-get update
+sudo apt-get install mongodb-org
+
+echo "Install node"
+sudo apt-get install -y npm
+sudo npm cache clean -f
+sudo npm install -g n
+sudo n stable
+
+echo "Install git"
+sudo apt-get install -y git
+
+echo "Install bower"
+sudo npm install -g bower
+
+echo "Install grunt"
+sudo npm install -g grunt-cli
+
+echo "Install meteor"
+curl https://install.meteor.com/ | sh
+
+#Meteor conf
+# run this command before starting meteor 
+# =========================================================
+# export MONGO_URL=mongodb://localhost:27017/dbname
+# =========================================================
 
 echo "Complete."
